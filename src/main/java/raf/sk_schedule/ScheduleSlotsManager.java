a6cded1f54df644ca5f92ff39402e4d2f33c88d1
Tadija <tadijasimic999@gmail.com>
2023-11-20 08:49:02 +0100

    BIG UPDATE SO UPDATING BASED ON MAIN DEPENDENCY UPDATE

--- src/main/java/raf/sk_schedule/ScheduleSlotsManager.java
+++ src/main/java/raf/sk_schedule/ScheduleSlotsManager.java
@@ -1,27 +1,32 @@
 package raf.sk_schedule;
 
-import raf.sk_schedule.api.ScheduleManager;
+import raf.sk_schedule.api.Constants.WeekDay;
+import raf.sk_schedule.api.ScheduleManagerAdapter;
 import raf.sk_schedule.exception.ScheduleException;
-import raf.sk_schedule.exception.ScheduleIOException;
-import raf.sk_schedule.filter.SearchCriteria;
-import raf.sk_schedule.model.RoomProperties;
-import raf.sk_schedule.model.ScheduleSlot;
-import raf.sk_schedule.util.exporter.ScheduleExporter;
+
+import raf.sk_schedule.model.location_node.RoomProperties;
+import raf.sk_schedule.model.schedule_mapper.RepetitiveScheduleMapper;
+import raf.sk_schedule.model.schedule_node.ScheduleSlot;
+import raf.sk_schedule.util.exporter.ScheduleExporterCSV;
+import raf.sk_schedule.util.exporter.ScheduleExporterJSON;
+import raf.sk_schedule.util.filter.CriteriaFilter;
+import raf.sk_schedule.util.filter.SearchCriteria;
 import raf.sk_schedule.util.importer.ScheduleImporter;
 import raf.sk_schedule.util.persistence.ScheduleFileWriter;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
-public class ScheduleSlotsManager implements ScheduleManager {
+import static raf.sk_schedule.util.date_formater.DateTimeFormatter.parseDate;
+
+public class ScheduleSlotsManager extends ScheduleManagerAdapter {
 
     private Date startingDate;
-
     private Date endingDate;
     private List<ScheduleSlot> mySchedule;
+
+    private List<RepetitiveScheduleMapper> repetitiveSchedule;
     private Map<String, RoomProperties> rooms;
 
 
@@ -31,35 +36,16 @@ public class ScheduleSlotsManager implements ScheduleManager {
     }
 
 
-    @Override
-    public void initialize(String startingDate, String endDate) {
-        try {
-            this.startingDate = new SimpleDateFormat(dateFormat).parse(startingDate);
-            this.endingDate = new SimpleDateFormat(dateFormat).parse(endDate);
-
-        } catch (ParseException e) {
-            throw new ScheduleException("Start or end date is in wrong format!");
-        }
-
-    }
-
-    public int loadRoomsSCV(String csvPath) throws IOException {
+    public int loadRoomsSCV(String csvPath) {
         rooms = ScheduleImporter.importRoomsCSV(csvPath);
         return rooms.size();
     }
 
-    public int loadScheduleSCV(String csvPath) throws IOException {
+    public int loadScheduleSCV(String csvPath) {
         if (rooms.isEmpty())
             throw new ScheduleException("Your room properties are currently empty. You need to import them first in order to bind the scheduled slots with their location.");
 
-        try {
-            mySchedule = ScheduleImporter.importScheduleCSV(csvPath, rooms);
-        } catch (ParseException e) {
-            throw new ScheduleException(e);
-        }
-
-        System.out.println("start " + startingDate.getTime() + "  end: " + endingDate.getTime() + "loaded rows:  " + mySchedule.size());
-        mySchedule.removeIf(slot -> slot.getStart().getTime() < startingDate.getTime() || slot.getStart().getTime() >= endingDate.getTime());
+        mySchedule = ScheduleImporter.importScheduleCSV(csvPath, rooms);
         return mySchedule.size();
     }
 
@@ -86,7 +72,7 @@ public class ScheduleSlotsManager implements ScheduleManager {
     public void updateRoom(String name, RoomProperties newProp) {
 
         if (!rooms.containsKey(name))
-            throw new ScheduleException("Room with a name: " + name + "does not exist in schedule.");
+            throw new ScheduleException("Room with a name: " + name + " does not exist in schedule.");
 
         if (!name.equals(newProp.getName())) {
 
@@ -94,7 +80,6 @@ public class ScheduleSlotsManager implements ScheduleManager {
                 throw new ScheduleException("You can not change room: " + name + " to " + newProp + " because room with that name already exists.\n" +
                         "If you really want to make this change you can change the room: " + newProp.getName() + " name to something else, than set room:. " + name + " to " + newProp.getName());
         }
-
         rooms.put(name, newProp);
     }
 
@@ -103,7 +88,6 @@ public class ScheduleSlotsManager implements ScheduleManager {
 
         if (!rooms.containsKey(s))
             throw new ScheduleException("There is no room with that name in schedule.");
-
         rooms.remove(s);
     }
 
@@ -120,122 +104,189 @@ public class ScheduleSlotsManager implements ScheduleManager {
         for (ScheduleSlot curr : mySchedule) {
             if (curr.isCollidingWith(timeSlot) && curr.getLocation().getName().equals(timeSlot.getLocation().getName()))
                 throw new ScheduleException(
-                        "The room: "
-                                + curr.getLocation().getName()
-                                + " is already scheduled between "
-                                + curr.getStartAsString().split(" +")[1]
-                                + " and " + curr.getEndAsString().split("\\s")[1]
-                                + " on: " + curr.getStartAsString().split(" +")[0]);
+                        "The room: " + curr.getLocation().getName()
+                                + " is already scheduled between " + curr.getStartTime()
+                                + " and " + curr.getEndTime()
+                                + " on date: " + curr.getDate()
+                );
         }
         return mySchedule.add(timeSlot);
     }
 
     @Override
-    public boolean scheduleRepetitiveTimeSlot(String s, String s1, long l, String s2) {
+    public boolean scheduleRepetitiveTimeSlot(String startTime, int duration, String endTime, WeekDay weekDay, int recurrencePeriod, String schedulingIntervalStart, String schedulingIntervalEnd) {
+        RepetitiveScheduleMapper.Builder mapperBuilder = new RepetitiveScheduleMapper.Builder()
+                .setStartTime(startTime)
+                .setRecurrenceIntervalStart(parseDate(schedulingIntervalStart))
+                .setRecurrenceIntervalEnd(parseDate(schedulingIntervalEnd));
+        if (endTime != null)
+            mapperBuilder.setEndTime(endTime);
+        else if (duration > 0)
+            mapperBuilder.setDuration(duration);
+
+        if (weekDay != null)
+            mapperBuilder.setWeekDay(weekDay);
+        if (recurrencePeriod > 0)
+            mapperBuilder.setRecurrencePeriod(recurrencePeriod);
+        return false;
+    }
+
+    @Override
+    public boolean scheduleRepetitiveTimeSlot(RepetitiveScheduleMapper repetitiveScheduleMapper) {
         return false;
     }
 
 
     @Override
     public boolean deleteTimeSlot(ScheduleSlot timeSlot) {
-
-
-        for (ScheduleSlot curr : mySchedule) {
-            if (curr.getStart().getTime() == timeSlot.getEnd().getTime() &&
-                    curr.getDuration() == timeSlot.getDuration())
-                return mySchedule.remove(curr);
-        }
-
         return false;
     }
 
-    @Override
     public void moveTimeSlot(ScheduleSlot oldTimeSlot, ScheduleSlot newTimeSlot) {
+
     }
 
     @Override
     public boolean isTimeSlotAvailable(ScheduleSlot timeSlot) {
-        for (ScheduleSlot curr : mySchedule) {
-            try {
-                if (curr.isCollidingWith(timeSlot))
-                    return false;
-            } catch (ParseException e) {
-                throw new ScheduleException(e);
-            }
+
+        for (ScheduleSlot slot : mySchedule) {
+            if (slot.isCollidingWith(timeSlot))
+                return false;
         }
         return true;
+
+    }
+
+    public boolean isTimeSlotAvailable(String date, String startTime, String endTime, String location) {
+        /* slots are bound by location dimension */
+        if (!rooms.containsKey(location))
+            throw new ScheduleException("Schedule model does not contain the room with the name: " + location + ".");
+
+        /*
+         Build out the slot, so we can easily check if there is collision,
+         and if not that means that the slot is available for scheduling
+         */
+        ScheduleSlot slot = new ScheduleSlot.Builder()
+                .setDate(parseDate(date))
+                .setStartTime(startTime)
+                .setEndTime(endTime)
+                .setLocation(rooms.get(location))
+                .build();
+
+        for (ScheduleSlot curr : mySchedule) {
+            if (slot.isCollidingWith(slot))
+                return false;
+        }
+        return true;
+
     }
 
 
     @Override
     public List<ScheduleSlot> getFreeTimeSlots(String startDate, String endDate) {
-        List<ScheduleSlot> freeTimeSlots = new ArrayList<>();
-        return freeTimeSlots;
 
+        throw new RuntimeException("NIJE GOTOVOOOOOOOOOOOOOOOOOO x,X' !");
     }
 
 
     @Override
     public List<ScheduleSlot> searchTimeSlots(SearchCriteria criteria) {
-        return null;
+        List<ScheduleSlot> modelState = new ArrayList<>(mySchedule);
+        /*
+        I can ignore the return value of filter() method call because the passed modelState list
+        is automatically changed  via reference that is sent as an argument
+        */
+        criteria.filter(modelState);
+
+        /* return computed list */
+        return modelState;
     }
 
 
     @Override
     public int exportScheduleCSV(String filePath, String firstDate, String lastDate) {
 
-        return 1;
-    }
-
-    @Override
-    public int exportFilteredScheduleCSV(String filePath, SearchCriteria searchCriteria, String date, String date1) {
-
-        return 1;
-    }
-
-
-    @Override
-    public int exportScheduleJSON(String filePath, String firstDate, String lastDate) {
-
+        /* using my own getSchedule() to bound exporting schedule, so I don't have to write same thing twice :v) */
         List<ScheduleSlot> schedule = getSchedule(firstDate, lastDate);
-        try {
-            File file = ScheduleFileWriter.createFileIfNotExists(filePath);
-            String serializedList = ScheduleExporter.listToJSON(schedule);
-            ScheduleFileWriter.writeStringToFile(file, serializedList);
-            return schedule.size();
 
-        } catch (ParseException | ScheduleException | ScheduleIOException e) {
-            throw new ScheduleException(e);
+        /* Export to file... */
+        File file = ScheduleFileWriter.initializeFile(filePath);
 
-        }
+        /* ScheduleComponentAPI Util default csv serialization */
+        String serializedList = ScheduleExporterCSV.listToCSV(schedule);
+
+        ScheduleFileWriter.writeStringToFile(file, serializedList);
+
+        /* return number of exported rows */
+        return schedule.size();
+    }
+
+
+    @Override
+    public int exportFilteredScheduleCSV(String filePath, SearchCriteria searchCriteria) {
+        return 1;
+    }
+
+
+    @Override
+    public int exportScheduleJSON(String filePath, String lowerDateBound, String upperDateBound) {
+
+        // configure file
+        File file = ScheduleFileWriter.initializeFile(filePath);
+
+        // extract the data
+        List<ScheduleSlot> schedule = getSchedule(
+                lowerDateBound == null ? super.startingDate : parseDate(lowerDateBound),
+                upperDateBound == null ? super.endingDate : parseDate(upperDateBound)
+        );
+
+        //serialize data
+        String serializedList = ScheduleExporterJSON.serializeObject(schedule);
+
+        //persist serialize data inside the file
+        ScheduleFileWriter.writeStringToFile(file, serializedList);
+
+        //return serialized objects count
+        return schedule.size();
+
     }
 
     @Override
-    public int exportFilteredScheduleJSON(String filePath, SearchCriteria searchCriteria, String date, String date1) {
-        return 1;
+    public int exportFilteredScheduleJSON(String filePath, SearchCriteria searchCriteria) {
 
+        // configure file
+        File file = ScheduleFileWriter.initializeFile(filePath);
+
+        // extract the data
+        List<ScheduleSlot> searchResult = searchTimeSlots(searchCriteria);
+
+        //serialize data
+        String serializedList = ScheduleExporterJSON.serializeObject(searchResult);
+
+        //persist serialize data inside the file
+        ScheduleFileWriter.writeStringToFile(file, serializedList);
+
+        //return serialized objects count with null ptr exception safety
+        return searchResult != null ? searchResult.size() : 0;
     }
 
     @Override
     public List<ScheduleSlot> getSchedule(String lowerBoundDate, String upperBoundDate) {
-        List<ScheduleSlot> result = new ArrayList<>(mySchedule);
-        try {
-            Date from = lowerBoundDate == null ? startingDate : new SimpleDateFormat(dateFormat).parse(lowerBoundDate);
-            Date until = upperBoundDate == null ? endingDate : new SimpleDateFormat(dateFormat).parse(upperBoundDate);
-            System.out.println(from + " MUHAMED ALI " + until);
-            result.removeIf(slot -> {
-
-                boolean yesSir = slot.getStart().getTime() < from.getTime() || slot.getStart().getTime() >= until.getTime();
-                System.out.println("YESIRRRRR : " + yesSir);
-                return yesSir;
-            });
-            return result;
-
-        } catch (ParseException e) {
-            throw new ScheduleException(e);
-        }
+        return new SearchCriteria.Builder()
+                .setCriteria(CriteriaFilter.LOWER_BOUND_DATE_KEY, lowerBoundDate)
+                .setCriteria(CriteriaFilter.UPPER_BOUND_DATE_KEY, upperBoundDate)
+                .build()
+                .filter(new ArrayList<>(mySchedule));
     }
 
+    @Override
+    public List<ScheduleSlot> getSchedule(Date lowerBoundDate, Date upperBoundDate) {
+        return new SearchCriteria.Builder()
+                .setCriteria(CriteriaFilter.LOWER_BOUND_DATE_KEY, lowerBoundDate)
+                .setCriteria(CriteriaFilter.UPPER_BOUND_DATE_KEY, upperBoundDate)
+                .build()
+                .filter(new ArrayList<>(mySchedule));
+    }
 
     @Override
     public List<ScheduleSlot> getWholeSchedule() {
